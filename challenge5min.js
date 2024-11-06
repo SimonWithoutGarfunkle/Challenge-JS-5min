32c5d77f555d3b3d8c1ac566aa51ad74a386baf5
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2024-11-06 15:17:37 +0100

    day 6

--- challenge5min.js
+++ challenge5min.js
@@ -2,14 +2,10 @@ document.getElementById("magicButton").addEventListener("click", function() {
     document.getElementById("secret").classList.toggle('secret');
 });
 
-const incomeList = [
-    { name: 'john', income: '32500' },
-    { name: 'joe', income: '54000' },
-    { name: 'hames', income: '42000' },
-]
+const seconds = 325
 
-function computeAverage(arr) {
-    return Math.floor(arr.reduce((sum, user) => sum += parseInt(user.income),0 )/arr.length);
+function secondToMin(sec) {
+    return `\"${Math.floor(sec/60)}:${sec%60}\"`;
 };
 
-console.log(computeAverage(incomeList));
+console.log(secondToMin(seconds));
