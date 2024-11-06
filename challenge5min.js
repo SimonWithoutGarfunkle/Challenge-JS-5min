a1465c5129e34b77b68beea09fca27279b61aaa6
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2024-11-06 14:53:49 +0100

    day 5

--- challenge5min.js
+++ challenge5min.js
@@ -2,19 +2,14 @@ document.getElementById("magicButton").addEventListener("click", function() {
     document.getElementById("secret").classList.toggle('secret');
 });
 
+const incomeList = [
+    { name: 'john', income: '32500' },
+    { name: 'joe', income: '54000' },
+    { name: 'hames', income: '42000' },
+]
 
+function computeAverage(arr) {
+    return Math.floor(arr.reduce((sum, user) => sum += parseInt(user.income),0 )/arr.length);
+};
 
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min))
-}
-
-function getRandomNumberRGB() {
-    return getRandomNumber(0, 255)
-}
-
-function generateRandomRGB() {
-    console.log(`rgb(${getRandomNumberRGB()}, ${getRandomNumberRGB()}, ${getRandomNumberRGB()})`)
-}
-
-
-generateRandomRGB();
+console.log(computeAverage(incomeList));
